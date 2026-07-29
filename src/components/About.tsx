168d0d3f536fd0de27d026c6fc46bca630d15c71
MD HABIBUR RAHMAN JESAN <habibur.rahman.jesan@gmail.com>
2026-07-29 18:07:20 +0600

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -116,7 +116,7 @@ const About = () => {
           >
             <motion.div variants={itemVariants}>
               <h3 className='text-2xl font-bold text-gray-900 dark:text-white mb-4'>
-                Hi, I'm Khairul Haque
+                Hi, I'm Jesan .
               </h3>
               <p className='text-gray-600 dark:text-gray-400 mb-4'>
                 With  1 years of experience in Software development, I
